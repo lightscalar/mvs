a4b56629ed96b828992fa0486f1807ed1b3e503a
Oliver <oliverhi@umich.edu>
2017-08-14 18:30:36 -0400

    Integrated the scheduler into the master_controller.

--- mvs/master_controller.py
+++ mvs/master_controller.py
@@ -1,10 +1,11 @@
 import threading
 from time import sleep, time
+from scheduler import Scheduler
 
 class MasterController(threading.Thread):
 
-    def __init__(self, module, database):
 
+    def __init__(self, module, database):
         # We'll run in a separate thread.
         threading.Thread.__init__(self)
 
@@ -13,11 +14,13 @@ class MasterController(threading.Thread):
         self.db = database
         self.keep_going = True
 
+
     def run(self):
         '''Start the controller.'''
         while self.keep_going:
             self.execute_command()
             sleep(1)
+
 
     def execute_command(self):
         '''Look for an active command in the database.'''
@@ -29,9 +32,30 @@ class MasterController(threading.Thread):
             #     # Assume we have: 'delta' as [dx, dy, dz]
             #     self.module.move(command['delta'])
 
+
     def update_schedule(self):
-        '''Go to database; find targets; construct schedule.'''
-        pass
+        # Add all available targets to the current schedule.
+        targets = self.db.targets
+        query = {
+            'active': True,
+            'module_id': self.module.module_id,
+            'end_time': { '$gt': time() }
+        }
+        cursor = targets.find(query)
+
+        for target in cursor:
+            # Create a list containing all of the times
+            # to take images at the targets location.
+            total_time = target['end_time'] - target['start_time']
+            times = []
+            i = 0
+            for _ in range(total_time / target['update_freq']):
+                image_time = target['start_time'] + (target['update_freq'] * i)
+                times.append((image_time, target['_id']))
+
+            self.scheduler.add_location(times)
+
+
 
     def stop(self):
         '''Stop the thread loop.'''
---
+++ mvs/scheduler.py
@@ -0,0 +1,81 @@
+# Written by Oliver Hill <oliverhi@umich.edu>
+# For Michigan Aerospace Corporation, for the MVS Microscope project.
+
+import time
+import threading
+
+class Scheduler(threading.Thread):
+    # A threaded component to maintain the motor
+    # scheduling for capturing images.
+
+    def __init__(self, module):
+        self.schedule = []
+        self.module = module
+        self.wait = False
+        self.restart = False
+        self.first = True
+
+
+    def run(self):
+        self.stop = False
+
+        while True:
+            for item in self.schedule:
+
+                # Get the coordinates from the id of the next target.
+                destination = self.module.get_location(item[1])
+                # Move to the destination.
+                self.module.move(destination)
+                #self.current = self.motor.get_location()
+
+                while time.time() < (item[0] + 1) or self.wait:
+                    # While the current time is less than the time that
+                    # the image will be taken plus one second, wait
+                    # and potentially get more instructions.  Also
+                    # wait for auto mode if it is in manual.
+                    if self.restart:
+                        # If a location gets added, restart the loop.
+                        break
+                if self.restart:
+                    break
+
+            self.schedule = []
+
+
+    def add_location(self, location):
+        # Add the times for a new location to the schedule,
+        # adding a "Image Captured" attribute.
+        # PRE-REQUISITE STRUCTURE:
+        #   ( <TIME TO TAKE IMAGE>, <TARGET ID> ).
+        for item in location:
+            self.schedule.append((item[0], item[1], False))
+
+        # Iterate over a copy of the schedule,
+        # and remove any times from the schedule which have already passed.
+        for item in list(self.schedule):
+            if item[0] < time.time():
+                self.schedule.remove(item)
+
+        # Finally, sort the list by time, leaving us with
+        # a schedule of locations to visit.
+        self.schedule = sorted(self.schedule, lambda x: x[0])
+
+        self.restart = True
+
+        if self.first:
+            # Start the schedule.
+            self.start()
+            self.first = False
+
+
+    def manual(self):
+        self.wait = True
+
+
+    def auto(self):
+        self.wait = False
+
+
+
+
+# -----------------------------------------------------------------------------
